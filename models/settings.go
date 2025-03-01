22990e190317c18a0ed3d0efae363d62f893ef5a
Alessandro-Maria-Caminiti <caminiti.2049247@studenti.uniroma1.it>
2025-03-01 15:59:49 +0100

    Delete models directory

--- models/settings.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// Settings represents the user settings
-type Settings struct {
-	Theme         string `json:"theme"`
-	Notifications bool   `json:"notifications"`
-}
--- models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// UserRequest represents the structure of a user login request.
-type UserRequest struct {
-	Name string `json:"name"`
-}
-
